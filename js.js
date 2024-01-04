89c47396d120b9dc025a651ae1bbc925e799a3e8
azzedineTech <116234372+AzzzedineTech@users.noreply.github.com>
2024-01-04 20:36:09 +0100

    responde-website

---
+++ js.js
@@ -0,0 +1,12 @@
+const Hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+Hamburger.addEventListener('click', () => {
+    Hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+    Hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}));
